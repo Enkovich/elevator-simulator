fcea36b239771a2757185f44526b2d066272d4a5
Enkovich <duma01.ru@mail.ru>
2022-10-03 19:26:00 +0300

    init repository

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  Vue JS
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+});
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
